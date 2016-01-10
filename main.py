25fd86887922e9d778b92d9c5d16ffc97028f187
Joshua Shi <thehandsomepanther@gmail.com>
2016-01-10 14:58:53 -0600

    included random

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
 from munkres import Munkres
-import sys
+import random
+from print_matrix import *
 import json
-
-def print_matrix(matrix, msg=None):
-    """
-    Convenience function: Displays the contents of a matrix of integers.
-    :Parameters:
-        matrix : list of lists
-            Matrix to print
-        msg : str
-            Optional message to print before displaying the matrix
-    """
-    import math
-
-    if msg is not None:
-        print(msg)
-
-    # Calculate the appropriate format width.
-    width = 1
-    for row in matrix:
-        for val in row:
-            if abs(val) > 1:
-               width = max(width, int(math.log10(abs(val))) + 1)
-
-    # Make the format string
-    format = '%%%dd' % width
-
-    # Print the matrix
-    for row in matrix:
-        sep = '['
-        for val in row:
-            sys.stdout.write(sep + format % val)
-            sep = ', '
-        sys.stdout.write(']\n')
 
 def main():
     m = Munkres()
@@ -74,7 +43,7 @@ def main():
                             repeats = 0
                             if data[person_a[:-2]]["matches"][key] == person_b[:-2]:
                                 repeats += 1
-                            cost = repeats * scale
+                            cost = repeats * scale + random.random() * scale
                     else:
                         cost = 100
                 else:
