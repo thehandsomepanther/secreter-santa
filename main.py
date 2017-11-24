30ed21ce6a04eb27ca6e500fd9b23576c54b9705
Josh Shi <thehandsomepanther@gmail.com>
2017-11-23 20:33:52 -0600

    add debug mode

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from credentials import *
 import json
 import smtplib
 from smtplib import SMTPException
+import sys
 
-def main():
+def main(debug=False):
     m = Munkres()
 
     A = []
@@ -20,17 +21,17 @@ def main():
         for person in data:
             years = []
 
-            for key in data[person]["matches"].keys():
+            for key in data[person]['matches'].keys():
                 years.append(int(key))
 
             last_year = max(last_year, max(years))
 
             santas.append(person)
 
-            person_a = person + "_A"
+            person_a = person + '_A'
             A.append(person_a)
 
-            person_b = person + "_B"
+            person_b = person + '_B'
             B.append(person_b)
 
         matrix = [[0 for x in range(len(A))] for x in range(len(A))]
@@ -44,10 +45,10 @@ def main():
                 scale = 100 / last_year
 
                 if person_a != person_b:
-                    if data[person_a]["matches"][str(last_year - 1)] != person_b:
+                    if data[person_a]['matches'][str(last_year - 1)] != person_b:
                         repeats = 0
-                        for key in data[person_a]["matches"].keys():
-                            if data[person_a]["matches"][key] == person_b:
+                        for key in data[person_a]['matches'].keys():
+                            if data[person_a]['matches'][key] == person_b:
                                 repeats += 1
                         cost = repeats * scale + random.random() * scale
                     else:
@@ -60,36 +61,45 @@ def main():
         indices = m.compute(matrix)
 
         # Munkres matrix
-        # print_matrix(matrix, msg='Secret Santa costs: ')
-        total = 0
+        if debug:
+            print_matrix(matrix, msg='Secret Santa costs: ')
 
+        total = 0
         for row, column in indices:
             value = matrix[row][column]
             total += value
 
             # Santa -(cost)-> Assignee
-            # print "{} -({})-> {}".format(santas[row], value, santas[column])
+            if debug:
+                print '{} -({})-> {}'.format(santas[row], value, santas[column])
+            else:
+                receiver = data[santas[row]]['email']
+                message = """
+                From: {}
+                To: {}
+                Subject: Secret Santa Assignment
 
-            receiver = data[santas[row]]["email"]
+                Ho, ho, ho. You have been assigned {}.
+                """.format(email, receiver, santas[column])
 
-            message = """
-            From: {}
-            To: {}
-            Subject: Secret Santa Assignment
-
-            Ho, ho, ho. You have been assigned {}.
-            """.format(email, receiver, santas[column])
-
-            try:
-                session = smtplib.SMTP('smtp.gmail.com', 587)
-                session.ehlo()
-                session.starttls()
-                session.login(email, password)
-                session.sendmail(email, receiver, message)
-                session.close()
-            except SMTPException:
-                print "Error: unable to send email"
+                try:
+                    session = smtplib.SMTP('smtp.gmail.com', 587)
+                    session.ehlo()
+                    session.starttls()
+                    session.login(email, password)
+                    session.sendmail(email, receiver, message)
+                    session.close()
+                except SMTPException:
+                    print 'Error: unable to send email'
 
         print 'Secret Santa assignment done.'
-        # print 'total cost: %d' % total
-main()
+        if debug:
+            print 'total cost: %d' % total
+
+if __name__ == '__main__':
+    debug = False
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--debug':
+        debug = True
+
+    main(debug)
